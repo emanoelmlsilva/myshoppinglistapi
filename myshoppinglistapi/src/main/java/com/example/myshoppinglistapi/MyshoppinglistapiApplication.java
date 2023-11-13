210a4090c917a2ec52ff27e3b601f44a46b1ea87
emanoel <emanoel.silva83@dcx.ufpb.br>
2023-11-12 22:00:37 -0300

    criado projeto com java

---
+++ myshoppinglistapi/src/main/java/com/example/myshoppinglistapi/MyshoppinglistapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.myshoppinglistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshoppinglistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshoppinglistapiApplication.class, args);
+	}
+
+}
